05e9a840a12e6ad23b527856f6a98d53730f4332
sunilyadav122 <sunilkumaryadav16021@gmail.com>
2023-04-16 16:55:25 +0530

    fixed engines

--- data/database.js
+++ data/database.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-    mongoose
-  .connect("mongodb://127.0.0.1:27017/", {
-    dbName: "backendApi",
-  })
-  .then(() => {
-    console.log("DB CONNECTED");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-}
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "backendApi",
+    })
+    .then(() => {
+      console.log("DB CONNECTED");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = {connectDB}
+module.exports = { connectDB };
