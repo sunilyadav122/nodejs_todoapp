97c97c672e69af13ca5024d56fd612e09760be17
sunilyadav122 <sunilkumaryadav16021@gmail.com>
2023-04-17 23:10:12 +0530

    FIxed Cors

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ config({
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,  
-  })
-);
+app.use(cors());
 app.use("/api/v1", userRouter);
 app.use("/api/v1", taskRouter);
 app.use(errorMiddleware);
@@ -29,3 +23,8 @@ module.exports = app;
 
 // => /user/:userId ==> req.params
 // => /user?userId=1234567890  =>  req.query
+// {
+//   origin: [process.env.FRONTEND_URL],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,  
+// }
