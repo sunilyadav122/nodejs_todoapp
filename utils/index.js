fb7bd00b4ee35f8bfe622b2009e3005959eeeea4
sunilyadav122 <sunilkumaryadav16021@gmail.com>
2023-04-19 00:21:16 +0530

    Removed Cookie Properties

--- utils/index.js
+++ utils/index.js
@@ -7,8 +7,6 @@ const setCookie = (user, res, statusCode, message) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 60,
-      sameSite: process.env.NODE_ENV === 'Development' ? "lax" : "none",
-      secure: process.env.NODE_ENV === 'Development' ? false : true,
     })
     .json({
       success: true,
